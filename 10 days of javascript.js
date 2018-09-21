8e2c5b3737372effcd59a086d35d477171d9c304
sergio <sdfunes9@gmail.com>
2018-09-20 19:02:36 -0600

    Switch statement

--- 10 days of javascript.js
+++ 10 days of javascript.js
@@ -13,8 +13,8 @@ function factorial(n) {
 
 factorial(3); */
 
-//Conditional-statements
-function getGrade(score) {
+//Conditional-statements IF-ELSE
+/* function getGrade(score) {
     let grade;
     if (score <= 30 && score > 25) {
         grade = 'A';
@@ -40,4 +40,33 @@ function getGrade(score) {
         }
     }
     return grade;
+} */
+
+//Switch statement
+function getLetters(s) {
+    let letter;
+    let firstLetter = s.charAt(0);
+    let label1 = ('a'&& 'e'&& 'i'&& 'o'&& 'u');
+    let label2 = ('b'&& 'c'&& 'd'&& 'f'&& 'g');
+    let label3 = ('h'&& 'j'&& 'k'&& 'l'&& 'm');
+
+    switch (firstLetter) {
+        case label1:
+        console.log('A');       
+            return 'A';
+            break;
+        case label2:
+        console.log('B');
+            return 'B';
+            break;
+        case label3:
+        console.log('C');
+            return 'C';
+            break;
+        default:
+        console.log('D');
+            return 'D';
+    }
+    return letter;
 }
+getLetters('mguhbdfgt');
