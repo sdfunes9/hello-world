b4700075b480730aac98af549757d591eaab268f
sergio <sdfunes9@gmail.com>
2018-09-20 18:42:44 -0600

    conditional statements function

--- 10 days of javascript.js
+++ 10 days of javascript.js
@@ -1,5 +1,5 @@
 //Factorial
-let facto = 0;
+/* let facto = 0;
 function factorial(n) {
     if (n>0 && n<=1) {
         facto = 1;
@@ -11,4 +11,33 @@ function factorial(n) {
     
 }
 
-factorial(3);
+factorial(3); */
+
+//Conditional-statements
+function getGrade(score) {
+    let grade;
+    if (score <= 30 && score > 25) {
+        grade = 'A';
+    } else {
+        if (score <= 25 && score > 20) {
+            grade = 'B';
+        } else {
+            if (score <= 20 && score > 15) {
+                grade = 'C';
+            } else {
+                if (score <=15 && score > 10) {
+                    grade = 'D';
+                } else {
+                    if (score <= 10 && score > 5) {
+                        grade = 'E';
+                    }else{
+                        if (score <= 5 && score >0) {
+                            grade = 'F';
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return grade;
+}
