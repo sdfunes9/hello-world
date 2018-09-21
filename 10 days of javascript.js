d7d3587a259c0610dce52bcaad14c9bd9eea532f
sergio <sdfunes9@gmail.com>
2018-09-21 17:52:53 -0600

    2 versions to verify a positive number

--- 10 days of javascript.js
+++ 10 days of javascript.js
@@ -106,7 +106,7 @@ nums = [2,3,6,6,5,7,8,10];
 secondLargest(nums); */
 
 //Try, catch and finally
-function reverseString(s) {
+/* function reverseString(s) {
     try {
         let splitString = s.split("");
         let inverted = splitString.reverse();
@@ -119,4 +119,44 @@ function reverseString(s) {
     
 }
 
-reverseString(1234);
+reverseString(1234); */
+
+//Throw
+function isPositive(a) {
+  try {
+    if (a > 0) {
+        console.log('YES');
+        return 'YES';
+    } else {
+      if (a === 0) {
+        throw new Error("Zero Error");
+      }else{
+          if (a < 0) {
+            throw new Error('Negative Error');
+          }
+      }
+    }
+  } catch (error) {
+      console.log(error.message);
+  }
+}
+//Alternative version for "isPositive"
+function isPositiv(b) {
+    try {
+        if (Math.sign(b) == 0) {
+            throw new Error('Zero Error');
+        } else {
+            if (Math.sign(b) == -1) {
+                throw new Error('Negative Error');
+            } else {
+                console.log('YES');
+                return 'YES';
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+isPositiv(2);
+isPositiv(0);
+isPositiv(6);
